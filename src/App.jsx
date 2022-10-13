145dfbff2ed9d90e6e42ff2e8e8aee987a47b0f3
Jesusebz <48937476+jesusebzcp@users.noreply.github.com>
2022-10-13 18:29:54 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-
-    <h1>Very good</h1>
-
-  )
+  return <h1>FROM PULL REQUEST</h1>;
 }
 
-export default App
+export default App;
